504c7d25e7c6e2cd6eafa3e416534597b9a242b5
BrianBell422 <brianbell.uk@gmail.com>
2021-02-23 16:02:36 +0000

    Initial commit

---
+++ src/components/CountryDetail.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <div id="selected_country">
+    <h2>Country Name: {{ country.name }}</h2>
+    <p> Population: {{ country.population }}</p>
+    
+    <img class="lrg-flag" :src="country.flag"/>
+
+    <neighbouring-countries>
+    </neighbouring-countries>
+
+</div>
+</template>
+
+<script>
+import NeighbourCountry from './NeighbourCountry.vue';
+export default {
+  name: 'country-detail',
+  props: ['country', 'country.flag'],
+  components: {
+    'neighbouring-countries': NeighbourCountry
+  }
+}
+</script>
+
+<style lang="css" scoped>
+
+.lrg-flag {
+  height: 100px
+}
+
+</style>
---
+++ src/components/FavouriteListItem.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div id="favourite_countries">
+    <h2>Favourite Countries</h2>
+    <li>{{ country }}</li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'favourite-countries',
+  props: ['favourite-country']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
